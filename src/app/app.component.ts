bc5dd8ae667d74946a8e3a5dc437f085e5dc8ae8
zack <zbowker66@gmail.com>
2023-05-24 14:15:31 -0400

    init commit`
    got it workiong just need to make it nice

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { DictionaryService } from './dictionary.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'ziktionary';
+  data: any;
+  found: Boolean = false;
+
+  constructor(private dicService: DictionaryService) {}
+
+  ngOnInit(): void {}
+
+  getDefinition(val: any) {
+    const searchedWord = Object.values(val);
+    this.data = this.dicService.getDefintion(searchedWord).subscribe((data) => {
+      console.log(data);
+      this.data = data;
+      this.found = true;
+      // console.log(this.data);
+    });
+    // console.log(this.data);
+  }
+}
---
+++ src/app/dictionary.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+
+const API_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en';
+@Injectable({
+  providedIn: 'root',
+})
+export class DictionaryService {
+  constructor(private http: HttpClient) {}
+
+  realData: any;
+
+  getDefintion(val: any) {
+    return this.http.get<any>(`${API_URL}/${val}`).pipe(
+      map((res) => {
+        console.log(res);
+        return res;
+      })
+    );
+  }
+}

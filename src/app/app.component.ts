01757831171f08e8d6c133d8a258ddae619b3af5
zack <zbowker66@gmail.com>
2023-05-25 14:30:42 -0400

    ready to start styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,7 +23,7 @@ export class AppComponent implements OnInit {
       // console.log(data);
       this.data = data;
       this.found = true;
-      // console.log(this.data);
+      console.log(this.data);
     });
     // console.log(this.data);
   }
